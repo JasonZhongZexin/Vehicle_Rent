43ba750b8169963bcbfcc35fc4cf6a7b6ade7093
JasonZhongZexin <jason.zhongzexin@gmail.com>
2019-05-24 08:11:29 +1000

    Create addCar.php
    add car to cart

---
+++ addCar.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    $model=$_REQUEST['model'];
+    $file = './cars.xml';
+    $cars = simplexml_load_file($file);
+    header("Content-Type: text/html; charset=utf-8");
+    header("Cache-Control: no-cache");
+    foreach($cars as $car){
+        if(($car->model)==$model){
+            if(($car->availability)=="Y"){
+                $thumbnail = "./images/" . (string)$car->model . ".jpg";
+                $vehicle = (string)$car->model_year . "-". (string)$car->brand . "-" . (string)$car->model;
+                if(!isset($_SESSION['cart'])||empty($_SESSION['cart'])){
+                    $request_car = array('thumbnail'=>$thumbnail,'vehicle'=>$vehicle,'price_pre_day'=>(string)$car->price_per_day,'rental_days'=>1);
+                    $vehicles = array($vehicle=>$request_car);
+                    $_SESSION['cart']=$vehicles;
+                }else{
+                    $vehicles = $_SESSION['cart'];
+                    if(array_key_exists($vehicle,$vehicles)){
+                        $request_car = $vehicles[$vehicle];
+                        $request_car['rental_days'] = ($request_car['rental_days'])+1;
+                        $vehicles[$vehicle] = $request_car; 
+                        $_SESSION['cart']=$vehicles;
+                    }else{
+                        $request_car = array('thumbnail'=>$thumbnail,'vehicle'=>$vehicle,'price_pre_day'=>(string)$car->price_per_day,'rental_days'=>1);
+                        $vehicles[$vehicle] = $request_car;
+                        $_SESSION['cart']=$vehicles;
+                    }
+                }
+                echo "Add to the cart successfully.";
+                print_r($_SESSION['cart']);
+            }else{
+                echo "Sorry, the car is not available now. Please try other cars.";
+            }
+        }
+    }
+?>
+
