bd4593332b44c1daf501fd0b0a54b72fc1ca1d9c
JasonZhongZexin <jason.zhongzexin@gmail.com>
2019-06-02 11:05:05 +1000

    add a link to home page

--- process_email.php
+++ process_email.php
@@ -17,8 +17,8 @@
         $subject = "Renting confirmation"; 
         $from = "zexin.zhong-1@student.uts.edu.au";
         $content = "Thanks for renting cars from Hertz-UTS, the total cost is $cost<br><br>Detail are as follows:";
-        if(isset($_SESSION['cart'])){ 
-            foreach($_SESSION['cart'] as $car){
+        if(isset($_SESSION['reservation_cart'])){ 
+            foreach($_SESSION['reservation_cart'] as $car){
                 $content .="<br><br>";
                 $content .="Model: ".$car['vehicle']."<br>Mileage: ".$car['mileage']."<br>Fuel type: ".$car['fuel_type']."<br>Seats: ".$car['seats']."<br>Price pre day: $".$car['price_pre_day']."<br>Rent days: ".$car['rental_days']."<br>Description: ".$car['description']." ";
             }
@@ -29,7 +29,7 @@
         $headers .= "From: Hertz-UTS@uts.edu.au";
     if(($first_name!=NULL||$first_name!="")&& ($email!=NULL||$email!="")){
         mail($email,$subject,$content,$headers);
-        echo "<br> <div id=\"order_email\">Thanks for shopping with us. <br>Your order has been processed and a confirmation email has been send to $email</div>";
+        echo "<br> <div id=\"order_email\">Thanks for renting with us. <br>Your order has been processed and a confirmation email has been send to $email <br> Click <a href=\"index.php\"> Here </a> to start a new order.</div>";
     }
 ?>
 </p>
